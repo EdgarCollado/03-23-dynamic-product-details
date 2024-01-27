dab7ce35604f693bcb862510b065cf7d8dcb06f5
EdgarCollado <edgarcollado@pursuit.org>
2024-01-27 17:47:06 -0500

    added changes to ProductDetail file

--- src/Components/ProductDetail.js
+++ src/Components/ProductDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import productsData from "../data/products.json";
 
 function ProductDetail () {
@@ -7,11 +7,20 @@ function ProductDetail () {
 
     const productInfo = productsData.products[productId];
 
+    const leftId = parseInt(productId) - 1;
+    const rightId = parseInt(productId) + 1;
+
+    const leftInfo = productsData.products[leftId];
+    const rightInfo = productsData.products[rightId];
+
     return (
         <div>
-            <h1>Product Detail</h1>
+            <h1>Product Details</h1>
             <p>{productInfo.name}</p>
             <p>{productInfo.description}</p>
+            {leftInfo && <Link to={`/product/${leftId}`}><strong>{leftInfo.name}</strong></Link>}
+            <br />
+            {rightInfo && <Link to={`/product/${rightId}`}><strong>{rightInfo.name}</strong></Link>}
         </div>
     );
 }
